476603a6207acbf8148353820383715caa0c16ab
tomaboro <funnymotek@gmail.com>
2017-01-12 19:11:18 +0100

    Running models every minute bug fixed

--- app/src/main/java/pl/kit/context_aware/lemur/HeartDROID/HeartAlarmReciever.java
+++ app/src/main/java/pl/kit/context_aware/lemur/HeartDROID/HeartAlarmReciever.java
@@ -1,9 +1,13 @@
 package pl.kit.context_aware.lemur.HeartDROID;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import pl.kit.context_aware.lemur.FilesOperations.FilesOperations;
 
@@ -20,5 +24,16 @@ public class HeartAlarmReciever extends BroadcastReceiver {
         }
 
         Toast.makeText(context, "Heart run!", Toast.LENGTH_LONG).show();
+
+        Calendar c = Calendar.getInstance();
+        Long time = c.getTimeInMillis()+60*1000;
+
+        Intent intentAlarm = new Intent(context, HeartAlarmReciever.class);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(context,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+        Toast.makeText(context, "Alarm Scheduled for one minute", Toast.LENGTH_LONG).show();
     }
 }
