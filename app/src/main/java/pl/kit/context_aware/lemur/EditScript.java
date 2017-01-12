76871d0b8fa25c5c073980e703e4bf470eb0a59e
tomaboro <motek@student.agh.edu.pl>
2017-01-12 10:45:39 +0100

    Setting script name field added.

--- app/src/main/java/pl/kit/context_aware/lemur/EditScript.java
+++ app/src/main/java/pl/kit/context_aware/lemur/EditScript.java
@@ -1,6 +1,7 @@
 package pl.kit.context_aware.lemur;
 
 import android.app.DialogFragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -16,12 +17,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -45,6 +52,8 @@ import static java.security.AccessController.getContext;
 public class EditScript extends AppCompatActivity implements DayOfWeekPickerFragment.NoticeDialogDOWPFListener
         , ActionPickerFragment.NoticeDialogAPFListener, TimePickerFragment.NoticeDialogTPFListener {
 
+    EditText scriptName;
+
     private int hour;
     private int minute;
     private double time;
@@ -52,24 +61,29 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
     private Double latitude;
     private LinkedList<Integer> days;
     private LinkedList<Integer> actions;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
-    public void SetTimeOnClick(View v){
+    public void SetTimeOnClick(View v) {
         DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getFragmentManager(),"Time Picker");
+        newFragment.show(getFragmentManager(), "Time Picker");
     }
 
-    public void SetDayOnClick(View v){
+    public void SetDayOnClick(View v) {
         DialogFragment newFragment = new DayOfWeekPickerFragment();
-        newFragment.show(getFragmentManager(),"DayOfWeek Picker");
+        newFragment.show(getFragmentManager(), "DayOfWeek Picker");
     }
 
-    public void SetActionOnClick(View v){
+    public void SetActionOnClick(View v) {
         DialogFragment newFragment = new ActionPickerFragment();
-        newFragment.show(getFragmentManager(),"Action Picker");
+        newFragment.show(getFragmentManager(), "Action Picker");
     }
 
-    public void SetLocationOnClick(View v){
-        Toast.makeText(this,"TO DO",Toast.LENGTH_SHORT).show();
+    public void SetLocationOnClick(View v) {
+        Toast.makeText(this, "TO DO", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -85,10 +99,14 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_script);
         Toolbar toolbar = (Toolbar) findViewById(R.id.edit_script_toolbar);
+        scriptName = (EditText) findViewById(R.id.es_set_tiitle_sub);
         //toolbar.setTitle(getResources().getString(R.string.es_Script));
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -104,7 +122,7 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
             case R.id.action_save_script:
 
                 //Creating basic model containing all needed types and attributes
-                ModelCreator newModel = ModelCreator.createBasicModel("model",this); //TODO name should be given by user
+                ModelCreator newModel = ModelCreator.createBasicModel(scriptName.getText().toString(), this); //TODO name should be given by user
 
                 //Creating lists which are needed in Scheme
                 LinkedList<Xattr> attributesList = new LinkedList<>();
@@ -120,37 +138,37 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
                 DecisionExpression decision = null;
                 ActionExpression action = null;
 
-                if(!(time == -1)){
+                if (!(time == -1)) {
                     attributesList.add(newModel.getAttribute("time"));
 
-                    alsv = new ALSVExpression(newModel.getAttribute("time"),Double.toString(time));
+                    alsv = new ALSVExpression(newModel.getAttribute("time"), Double.toString(time));
                     ALSVList.add(alsv);
                 }
-                if (!days.isEmpty()){
+                if (!days.isEmpty()) {
                     attributesList.add(newModel.getAttribute("day"));
 
-                    final String []daysOfWeekArray = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
+                    final String[] daysOfWeekArray = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
                     LinkedList<String> selectedDays = new LinkedList<>();
-                    for(Integer day : days){
+                    for (Integer day : days) {
                         selectedDays.add(daysOfWeekArray[day]);
                     }
-                    alsv = new ALSVExpression(newModel.getAttribute("day"),selectedDays);
+                    alsv = new ALSVExpression(newModel.getAttribute("day"), selectedDays);
                     ALSVList.add(alsv);
                 }
-                if((longitude != null) && (latitude != null)){
+                if ((longitude != null) && (latitude != null)) {
                     attributesList.add(newModel.getAttribute("longitude"));
-                    alsv = new ALSVExpression(newModel.getAttribute("longitude"),longitude.toString());
+                    alsv = new ALSVExpression(newModel.getAttribute("longitude"), longitude.toString());
                     ALSVList.add(alsv);
 
                     attributesList.add(newModel.getAttribute("latitude"));
-                    alsv = new ALSVExpression(newModel.getAttribute("latitude"),latitude.toString());
+                    alsv = new ALSVExpression(newModel.getAttribute("latitude"), latitude.toString());
                     ALSVList.add(alsv);
                 }
-                if(!actions.isEmpty()){
+                if (!actions.isEmpty()) {
                     attributesToSetList.add(newModel.getAttribute("sound"));
 
-                    final String [] actionsArray = {"off","on","vibration"};
-                    for(Integer actionNumber : actions){
+                    final String[] actionsArray = {"off", "on", "vibration"};
+                    for (Integer actionNumber : actions) {
                         decision = new DecisionExpression(newModel.getAttribute("sound"), actionsArray[actionNumber]);
                         decisionList.add(decision);
                     }
@@ -159,11 +177,11 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
 
                 }
                 // Creating and adding scheme to model
-                Xschm scheme = new Xschm("SetSounds",attributesList,attributesToSetList);
+                Xschm scheme = new Xschm("SetSounds", attributesList, attributesToSetList);
                 newModel.addScheme(scheme);
                 //Creating Rules
                 LinkedList<Xrule> rulesList = new LinkedList<>();
-                Xrule rule = new Xrule(scheme,1,ALSVList,decisionList,actionList);
+                Xrule rule = new Xrule(scheme, 1, ALSVList, decisionList, actionList);
                 newModel.addRule(rule);
                 newModel.saveModel();
 
@@ -172,7 +190,7 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
                         Log.i("File ", file.toString());
                 }
                 */
-                Toast.makeText(this,getResources().getString(R.string.es_Added),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, getResources().getString(R.string.es_Added), Toast.LENGTH_SHORT).show();
                 finish();
                 return true;
 
@@ -204,13 +222,49 @@ public class EditScript extends AppCompatActivity implements DayOfWeekPickerFrag
 
     @Override
     public void onDialogTPFPositiveClick(DialogFragment dialog) {
-        hour = ((TimePickerFragment)dialog).getHour();
-        minute = ((TimePickerFragment)dialog).getMinute();
-        time = hour + (minute/60);
+        hour = ((TimePickerFragment) dialog).getHour();
+        minute = ((TimePickerFragment) dialog).getMinute();
+        time = hour + (minute / 60);
     }
 
     @Override
     public void onDialogTPFNegativeClick(DialogFragment dialog) {
 
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("EditScript Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
